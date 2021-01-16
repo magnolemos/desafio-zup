52a7330cf61906f9e0a1829bbc8cfb2d126ca9c4
Magno Lemos <magnolemos@MacBook-Pro-de-Magno.local>
2021-01-16 20:48:26 -0300

    feat:(data.service): service para pegar o layout.json

---
+++ desafio-zup/src/app/service/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getData(url: string): Observable<Object>
+  {
+      return this.httpClient.get(url)
+  }
+}
