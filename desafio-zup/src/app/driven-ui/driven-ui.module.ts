6129e2be34898b4551cd2547bce55f853d084060
Magno Lemos <magnolemos@MacBook-Pro-de-Magno.local>
2021-01-16 20:56:00 -0300

    feat:(app-root.component): add component root

---
+++ desafio-zup/src/app/driven-ui/driven-ui.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRootComponent } from './app-root/app-root.component';
+
+@NgModule({
+  declarations: [AppRootComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DrivenUiModule { }
