18e2f4083626108c3be3cd7572ab337218ae3869
Magno Lemos <magnolemos@MacBook-Pro-de-Magno.local>
2021-01-17 20:48:22 -0300

    fix:(imagem.component): extends  do BaseComponent

--- desafio-zup/src/app/driven-ui/components/image/image.component.ts
+++ desafio-zup/src/app/driven-ui/components/image/image.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BaseComponent } from '../base/base.component';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent extends BaseComponent implements OnInit {
 
   @Input() url!: string;
   @Input() description!: string;
   
-  constructor() { }
+  constructor() { 
+    super()
+  }
 
   ngOnInit(): void {
   }
