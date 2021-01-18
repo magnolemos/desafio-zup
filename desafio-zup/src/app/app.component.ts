8bf16df805abb941345c60135408e27acb7c73fb
Magno Lemos <magnolemos@MacBook-Pro-de-Magno.local>
2021-01-17 21:08:48 -0300

    feat:(app.component): fallback erro para buscar o json

--- desafio-zup/src/app/app.component.ts
+++ desafio-zup/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { DataService } from './service/data.service';
 })
 export class AppComponent implements OnInit {
   private URL = '/assets/layout.json';
+  public hasErro = false;
   public layout: any;
 
   constructor(private dataService: DataService) { }
   
   ngOnInit(): void {
-   this.dataService.getData(this.URL).subscribe((layout) => this.layout = layout)
+   this.dataService.getData(this.URL).subscribe((layout) => this.layout = layout, (error) => this.hasErro = true)
   }
 }
