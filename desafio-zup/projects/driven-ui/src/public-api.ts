a59689b0553646f55cad47a77637be2f6c835d24
Magno Lemos <magnolemos@MacBook-Pro-de-Magno.local>
2021-01-19 20:54:47 -0300

    feat:criado a lib e utilizado no app.component

---
+++ desafio-zup/projects/driven-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of driven-ui
+ */
+
+export * from './lib/driven-ui/driven-ui.module';
+export * from './lib/driven-ui/app-root/app-root.component';
--- desafio-zup/src/app/app.module.ts
+++ desafio-zup/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DrivenUiModule } from './driven-ui/driven-ui.module';
 import { HttpClientModule } from '@angular/common/http';
+import { DrivenUiModule } from 'projects/driven-ui/src/public-api';
+// import { DrivenUiModule } from 'projects/driven-ui/src/public-api';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DrivenUiModule,
-    HttpClientModule
+    HttpClientModule,
+    DrivenUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
